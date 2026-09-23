ec218296126945e9e46ff6f11eca2b731ce2559a
Mohsen-Khakzad <khakzadmohsen32@gmail.com>
2026-09-23 20:07:55 +0330

    feat: add scroll-based header resizing

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("header");
+addEventListener("scroll",() => {
+    if(window.scrollY > 0){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove("scrolled");
+    }
+});
